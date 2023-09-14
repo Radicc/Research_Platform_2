95bb548426f63f04884d1ba13f14875d3e0033b3
Tomas Radic <tomasradic00@gmail.com>
2023-09-14 22:15:44 +0200

    deploy

--- src/features/study_section/getStudyData.ts
+++ src/features/study_section/getStudyData.ts
@@ -1,5 +1,5 @@
 import { collection, getDocs } from "firebase/firestore"
-import DB from "@/config/firestore.js"
+import DB from "@/config/firestore.ts"
 
 const GetStudyData = () => {
   const db = DB()
